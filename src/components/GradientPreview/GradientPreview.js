7e01588142f9772352a68ffd98aa8ca58abde3d9
Arsen Yavorskyi <ars.yar24@gmail.com>
2024-05-22 10:29:02 +0300

    added css output

--- src/components/GradientPreview/GradientPreview.js
+++ src/components/GradientPreview/GradientPreview.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./gradient-preview.module.css";
 import { createGradientBackground } from "../../helpers";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function GradientPreview({ colors, angle, mode, bezierParam }) {
   // create an array with color enries that only have active: true property
@@ -14,7 +16,15 @@ function GradientPreview({ colors, angle, mode, bezierParam }) {
     bezierParam
   );
 
-  return <div style={gradientStyles} className={styles.wrapper}></div>;
+  const codeString = `.element {\n    background-image: ${gradientStyles.backgroundImage}\n}`;
+  return (
+    <>
+      <div style={gradientStyles} className={styles.wrapper}></div>
+      <SyntaxHighlighter language="css" style={dracula}>
+        {codeString}
+      </SyntaxHighlighter>
+    </>
+  );
 }
 
 export default GradientPreview;
